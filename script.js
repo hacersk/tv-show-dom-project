ac753557d94f41f0f7008d56a16b87dbd5fad148
hacersk <hacersk41@gmail.com>
2020-07-01 13:45:54 +0100

    level 100 is almost finished

--- script.js
+++ script.js
@@ -6,7 +6,65 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.style.backgroundColor = "RGB(237, 235, 230)";
+
+  let container = document.createElement("div");
+  container.className = "container";
+  rootElem.appendChild(container);
+  container.style.display ="flex";
+  container.style.flexWrap ="wrap";
+ 
+  container.style.width ="100%";
+  container.style.justifyContent="center";
+  //container.style.alignContent="center";
+  episodeList.forEach(element => {
+    console.log(element);
+    let article = document.createElement("article");
+    container.appendChild(article);
+
+   
+    //article.float ="left";
+    article.style.width= "25%";
+   article.style.margin ="20px 30px 10px 10px";
+    article.style.borderStyle ="outset";
+    article.style.backgroundColor ="white";
+   
+    article.style.padding ="15px";
+    article.style.color= "black";
+    article.style.textAlign ="center";
+
+
+
+    let h3 = document.createElement("h3");
+    h3.innerHTML = element.name +" - "+ "S0"+element.season+"SO"+element.number;
+    article.appendChild(h3);
+    //h3.style.boxShadow =" 3px 3px 0 #c9c9c7";
+    h3.style.padding="25px";
+    h3.style.borderStyle="solid";
+    h3.style.borderRadius ="5px";
+
+    let img = document.createElement("img");
+    img.src = element.image.medium;
+    article.appendChild(img);
+    img.style.marginBottom="5px";
+
+    let p = document.createElement("p");
+     p.textContent = element.summary;
+    article.appendChild(p);
+    p.style.textAlign="left";
+    p.style.paddingRight ="20px";
+    p.style.marginTop="15px";
+    p.style.opacity =".6";
+    
+    let a = document.createElement("a");
+    a.innerText = element._links.self.href;
+    article.appendChild(a);
+    a.style.marginTop = "5px";
+    a.style.opacity =".3";
+      
+  });
+ 
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
 window.onload = setup;
