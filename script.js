a8795842fe432ad44bf6f4d0520fe0aa0e118ecc
hacersk <hacersk41@gmail.com>
2020-07-01 23:35:47 +0100

    responsive and css.style added

--- script.js
+++ script.js
@@ -6,62 +6,53 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.style.backgroundColor = "RGB(237, 235, 230)";
+  //rootElem.style.backgroundColor = "RGB(237, 235, 230)";
 
   let container = document.createElement("div");
   container.className = "container";
   rootElem.appendChild(container);
-  container.style.display ="flex";
-  container.style.flexWrap ="wrap";
- 
-  container.style.width ="100%";
-  container.style.justifyContent="center";
-  //container.style.alignContent="center";
-  episodeList.forEach(element => {
+  
+    episodeList.forEach(element => {
     console.log(element);
     let article = document.createElement("article");
     container.appendChild(article);
 
-   
-    //article.float ="left";
-    article.style.width= "25%";
-   article.style.margin ="20px 30px 10px 10px";
-    article.style.borderStyle ="outset";
-    article.style.backgroundColor ="white";
-   
-    article.style.padding ="15px";
-    article.style.color= "black";
-    article.style.textAlign ="center";
-
-
-
+       
     let h3 = document.createElement("h3");
-    h3.innerHTML = element.name +" - "+ "S0"+element.season+"E0"+element.number;
+    let getNumber;
+    if(element.number >=10){
+      getNumber = "E"+element.number;}
+      else {
+        getNumber ="E0"+element.number; 
+      }
+    let getSeason;
+    if(element.season>10){
+      getSeason ="S"+element.season;
+    } else{
+      getSeason = "S0"+element.season;
+    } 
+      
+    h3.innerHTML = element.name +" - "+ getSeason+getNumber;
     article.appendChild(h3);
-    //h3.style.boxShadow =" 3px 3px 0 #c9c9c7";
-    h3.style.padding="25px";
-    h3.style.borderStyle="solid";
-    h3.style.borderRadius ="5px";
+   
 
     let img = document.createElement("img");
     img.src = element.image.medium;
     article.appendChild(img);
-    img.style.marginBottom="5px";
+   
 
     let p = document.createElement("p");
      p.textContent = element.summary;
     article.appendChild(p);
-    p.style.textAlign="left";
-    p.style.paddingRight ="20px";
-    p.style.marginTop="15px";
-    p.style.opacity =".6";
+   
     
     let a = document.createElement("a");
+    console.log(element._links.self.href);
+    a.title ="click";
+    a.href = element._links.self.href ;
     a.innerText = element._links.self.href;
     article.appendChild(a);
-    a.style.marginTop = "5px";
-    a.style.opacity =".3";
-      
+   
   });
  
   //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
